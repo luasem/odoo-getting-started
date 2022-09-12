7fd76e0becbaf3a664ef5eec545a7977f07e991b
Jaime Alonso <jaime@vauxoo.com>
2022-09-12 12:23:59 -0500

    Salesperson and Buyer added in Other Info tab

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -28,3 +28,5 @@ class Property(models.Model):
     )
     active = fields.Boolean(default=True)
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)

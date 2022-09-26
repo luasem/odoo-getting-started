ac2528aa7176d455ef6af383e52192b4fd5c29ba
Jaime Alonso <jaime@vauxoo.com>
2022-09-26 14:29:39 -0500

    Creation of a link module

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -5,4 +5,34 @@ class Property(models.Model):
     _inherit = "estate.property"
 
     def sell_property(self):
-        return super()
+        res = super().sell_property()
+        journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+        for record in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": record.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "journal_id": journal.id,
+                    "invoice_line_ids": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": record.name,
+                                "quantity": 1.0,
+                                "price_unit": record.selling_price * 0.06,
+                            },
+                        ),
+                        (
+                            0,
+                            0,
+                            {
+                                "name": "Administrative fees",
+                                "quantity": 1.0,
+                                "price_unit": 100.0,
+                            },
+                        ),
+                    ],
+                }
+            )
+        return res
